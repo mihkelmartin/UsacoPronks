5383584e0ebb7f0cd6503d6d0a1bfe3970d31986
mihkel.martin <mihkelmartin@gmail.com>
2021-09-15 10:44:10 +0300

    MEX

--- src/Kylaskaigud.java
+++ src/Kylaskaigud.java
@@ -13,7 +13,7 @@ public class Kylaskaigud {
         String sisend[] = in.readLine().split(" ");
         int maju = Integer.parseInt(sisend[0]);
         int lapsi = Integer.parseInt(sisend[1]);
-        int[][] maja_sektsioon = new int[maju + 1][2]; // Maja koos samakõrgus grupi numbriga. Yks rohkem, et saaks alustada 1 indeksist
+        int[][] maja_grupp = new int[maju + 1][2]; // Maja koos samakõrgus grupi numbriga. Yks rohkem, et saaks alustada 1 indeksist
 
         StringTokenizer st = new StringTokenizer(in.readLine()," ");
         int maja_korgus_eelmine = -100001;
@@ -23,17 +23,26 @@ public class Kylaskaigud {
         TreeMap<Integer, Object> korgus_grupid_maps = new TreeMap<>();
 
         for (int i = 1; i <=maju ; i++) {
+
             int maja_korgus = Integer.valueOf(st.nextToken());
-            maja_sektsioon[i][0] = maja_korgus; // i-nda maja kõrgus
+            maja_grupp[i][0] = maja_korgus; // i-nda maja kõrgus
             korgus_grupi_pikkus++;
 
-            if(maja_korgus != maja_korgus_eelmine && i!=1){ // Ei ole esimene kord
+
+            // Et esimene kord ei läheks järgmisse if-i
+            if(i == 1){
+                maja_korgus_eelmine = maja_korgus;
+                korgus_grupi_pikkus = 0;
+            }
+
+
+            // Paneme tagantjärgi, kui grupp läbi
+            if(maja_korgus != maja_korgus_eelmine){ // Ei ole esimene kord
                 // Kui muutus kasvata korgus_gruppi
                 maja_korgus_eelmine = maja_korgus;
-
                 int[] korgus_grupi_andmed = new int[3];
                 korgus_grupi_andmed[0] = korgus_grupi_algus;
-                korgus_grupi_andmed[1] = i - 1; // lopp
+                korgus_grupi_andmed[1] = i - 1; // lopp, esimene kord ei tule seega -1 ok
                 korgus_grupi_andmed[2] = korgus_grupi_pikkus;
                 korgus_grupid_maps.put(korgus_grupp, korgus_grupi_andmed);
 
@@ -41,8 +50,24 @@ public class Kylaskaigud {
                 korgus_grupi_algus = i; // järgmise korgus_grupi algus
                 korgus_grupi_pikkus = 0;
             }
-            maja_sektsioon[i][1] = korgus_grupp;
+            // pane kõrgusgrupp
+            maja_grupp[i][1] = korgus_grupp;
         }
+        // Lisa viimane
+        int[] korgus_grupi_andmed = new int[3];
+        korgus_grupi_andmed[0] = korgus_grupi_algus;
+        korgus_grupi_andmed[1] = maju; // lopp
+        korgus_grupi_andmed[2] = ++korgus_grupi_pikkus;
+        korgus_grupid_maps.put(korgus_grupp, korgus_grupi_andmed);
+
+
+        // Nüüd tuleb teha kahendpuu Kõrgusgruppidest
+        // Sealt saame kiirelt teada mis on mingite gruppide pikimad lõigud - näiteks grupist 35 - 2300 pikim lõik 45
+        // Kui antakse sisend majade indeksitega, siis massiivist maja_grupp leiame kuhu gruppi vastava maja kuulub
+        // Kui on antud maja_algus ja maja_lopp, siis leiame nende grupid ning otsime maja_alguse grupist ühest suurema
+        // ja maja_lopp grupist ühe väiksema vahemikust. Sest äärtes võib olla mittetäielik kattumine, neid peab eraldi vaatama
+        // Selleks aga on meil korgus_grupid_maps TreeMap, kust saame vastavad andmed
+
 
         System.out.println("Total execution time: " + (System.currentTimeMillis() - startTime));
 
