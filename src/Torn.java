5b701f55f1731f5055a92e6caef226fbb4ce818c
mihkel.martin <mihkelmartin@gmail.com>
2021-09-17 13:39:11 +0300

    Kastid

--- src/Torn.java
+++ src/Torn.java
@@ -1,7 +1,6 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Map;
-import java.util.TreeMap;
+
 
 public class Torn {
     public static void main(String[] args )throws Exception {
@@ -10,39 +9,36 @@ public class Torn {
         int kaste = Integer.parseInt(in.readLine());
 
         int[][] kastid_kandevoimed = new int[kaste][2];
+        int max_kandevoime = 0;
         for (int i = 0; i < kaste; i++) {
             String sisend[] = in.readLine().split(" ");
             int kaal = Integer.parseInt(sisend[0]);
             int kandevoime = Integer.parseInt(sisend[1]);
             kastid_kandevoimed[i][0] = kaal;
             kastid_kandevoimed[i][1] = kandevoime;
+            max_kandevoime = kandevoime > max_kandevoime ? kandevoime : max_kandevoime;
         }
 
-        int parim_kaste = 0;
-        for (int i = 0; i < kaste; i++) {
-            int valitud_kasti_indeks = i;
-            int kandevoime = kastid_kandevoimed[i][1]; // Pane max algusesse
-            int max_kaste = 1;
-            while(true){
-                int vahepeale_parim_kandevoime = Integer.MIN_VALUE;
-                for (int j = valitud_kasti_indeks; j < kaste; j++) {
-                    int uus_kandevoime = Math.min(kastid_kandevoimed[j][1], kandevoime - kastid_kandevoimed[j][0]);
-                    if(uus_kandevoime > vahepeale_parim_kandevoime){
-                        vahepeale_parim_kandevoime = uus_kandevoime;
-                        valitud_kasti_indeks = j;
-                    }
-                }
-                kandevoime = vahepeale_parim_kandevoime;
-                if(kandevoime >= 0){
-                    max_kaste++;
-                } else {
-                    break;
-                }
+        // Selle massiivi indeksi i kohal on i kasti korral suurim kandevõime
+        // Näiteks kui i = 1, siis algses massiivis parim kandevõime
+        int[] parim_alles_kandevoime = new int[max_kandevoime + 1];
+        parim_alles_kandevoime[0] = Integer.MIN_VALUE;
+        parim_alles_kandevoime[1] = max_kandevoime;
+        // Alustame 2st ses 0 ja 1 on olemas
+        int result = 2;
+        for (int j = 2; j <= max_kandevoime; j++) {
+            for (int i = 0; i < kaste; i++) {
+                int parimast_eelmisest_kandevoimest_kasti_kaal_maha =parim_alles_kandevoime[j-1] - kastid_kandevoimed[i][0];
+                int kaesolev_kandevoime = kastid_kandevoimed[i][1];
+                // Kasi enda kandevõime võib olla väiksem kui allesjäänust maha lahutatud kaal
+                int alles_jaav_kandevoime = Math.min(parimast_eelmisest_kandevoimest_kasti_kaal_maha, kaesolev_kandevoime);
+                // Pane suurim
+                parim_alles_kandevoime[j] = Math.max(parim_alles_kandevoime[j], alles_jaav_kandevoime);
             }
-            if(max_kaste>parim_kaste)
-                parim_kaste = max_kaste;
-
+            if(parim_alles_kandevoime[j] != 0){
+                result++;
+            }
         }
-        System.out.println(parim_kaste);
+        System.out.println(result);
     }
 }
