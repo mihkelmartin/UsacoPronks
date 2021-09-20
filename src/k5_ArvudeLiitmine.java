fb8fe42b57cbf02df69ab3d3c3433106312a1f08
mihkel.martin <mihkelmartin@gmail.com>
2021-09-20 12:53:16 +0300

    Arvude liitmine

---
+++ src/k5_ArvudeLiitmine.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class k5_ArvudeLiitmine {
+    public static void main(String[] args )throws Exception {
+
+        final long mod = 1000000000 + 7;
+
+        InputStreamReader ina = new InputStreamReader(System.in);
+        BufferedReader in = new BufferedReader(ina);
+        String sisend[] = in.readLine().split(" ");
+        int K = Integer.parseInt(sisend[0]);
+        int N = Integer.parseInt(sisend[1]);
+
+        long result = 1;
+        if(K == 1){
+            System.out.println("1");
+        } else
+        if(K==2) {
+            System.out.println(N+1);
+        } else {
+            // SEE EI TÖÖTA - ERITI KUI K on suurem kui N näiteks, siis tekib 0 palju
+            // Lisaks mis siis kui K sama suur kui N või natuke väiksem
+            boolean bJagatudKahega = false;
+            while (K-- > 1) {
+                result = result * (N+2);
+                if(!bJagatudKahega && result%2 == 0){
+                    result /= 2;
+                    bJagatudKahega = true;
+                }
+                if(bJagatudKahega) {
+                    result %= mod;
+                }
+                N--;
+            }
+            System.out.println(result);
+        }
+    }
+}
