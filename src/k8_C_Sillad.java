6108a94485a406a6b7b32ba4ac2efbf9fbba4516
mihkel <mihkelmartin@gmail.com>
2021-09-25 12:17:05 +0300

    Sillad teine päev üsna hea

--- src/k8_C_Sillad.java
+++ src/k8_C_Sillad.java
@@ -42,7 +42,7 @@ public class k8_C_Sillad {
         // Dünaamilist teeme alt-üles, seega vasakult alt hakkame tulema ja vaatame kuidas paremal on sama usku
         // firmasid ja täidame tabelit. Firma nimi ei ole tegelikult oluline
         // Alusta eelviimasest, sest viimane on meil selleks, et oleks 0
-        int vastus = 0;
+        int vastus = 0, parima_i = vasak_firmasid, parim_j = parem_firmasid;
         for (int i = vasak_firmasid - 1; i >= 0; i--) {
             for (int j = parem_firmasid - 1; j >= 0; j--) {
                 Firma vasak = vasakfirmad.get(i);
@@ -53,7 +53,10 @@ public class k8_C_Sillad {
                             Math.max(vasak.tootlikkus + parem.tootlikkus + tootlikkuse_tabel[i + 1][j + 1],
                                     // kui pole suurem siis võib alusmisega jätkata alati
                                     tootlikkuse_tabel[i][j + 1]);
-                    vastus = Math.max(vastus, tootlikkuse_tabel[i][j]);
+                    if(tootlikkuse_tabel[i][j] > vastus){
+                        vastus = tootlikkuse_tabel[i][j];
+                        parima_i = i; parim_j = j;
+                    }
                 } else {
                     // Kui pole   mätsi võta enda eelmine või eelmise eelmine kui see suurem
                     tootlikkuse_tabel[i][j] = Math.max(tootlikkuse_tabel[i][j+1], tootlikkuse_tabel[i+1][j]);
@@ -61,10 +64,29 @@ public class k8_C_Sillad {
             }
         }
 
-        HashSet<Integer> sildu = new HashSet<>();
-        for (int i = 0; i < parem_firmasid; i++) {
-            sildu.add(tootlikkuse_tabel[0][i]);
+        // Üritame nüüd tagurpidi minnes aru saada mitu silda oli vaja
+        int vahepealne_parim = vastus;
+        int sildu = 0;
+        while (vahepealne_parim > 0) {
+            // Kõik variandid
+            if(tootlikkuse_tabel[parima_i][parim_j+1] == vahepealne_parim && tootlikkuse_tabel[parima_i+1][parim_j] != vahepealne_parim){
+                parim_j++;
+            } else if(tootlikkuse_tabel[parima_i][parim_j+1] != vahepealne_parim && tootlikkuse_tabel[parima_i+1][parim_j] == vahepealne_parim){
+                parima_i++;
+            } else if(tootlikkuse_tabel[parima_i][parim_j+1] != vahepealne_parim && tootlikkuse_tabel[parima_i+1][parim_j] != vahepealne_parim){
+                if(vahepealne_parim - tootlikkuse_tabel[parima_i][parim_j+1] > vahepealne_parim - tootlikkuse_tabel[parima_i+1][parim_j]){
+                    parim_j++;
+                } else {
+                    parima_i++;
+                }
+            } else if(tootlikkuse_tabel[parima_i][parim_j+1] == vahepealne_parim && tootlikkuse_tabel[parima_i+1][parim_j] == vahepealne_parim){
+                parima_i++;parim_j++;
+            }
+            if(tootlikkuse_tabel[parima_i][parim_j] != vahepealne_parim){
+                vahepealne_parim = tootlikkuse_tabel[parima_i][parim_j];
+                sildu++;
+            }
         }
-        System.out.println(vastus + " " + sildu.size());
+        System.out.println(vastus + " " + sildu);
     }
 }
