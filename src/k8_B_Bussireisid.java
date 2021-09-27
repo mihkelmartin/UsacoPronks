ef3d515c006b9fbc292b9a4154fa0228e583673f
mihkel <mihkelmartin@gmail.com>
2021-09-27 18:45:04 +0300

    Bussireisid hea variant

--- src/k8_B_Bussireisid.java
+++ src/k8_B_Bussireisid.java
@@ -36,8 +36,22 @@ public class k8_B_Bussireisid {
 
         // Esimene rida edasisõit, teine rida tagasisõit.
         // kuidas siis kui peatust polegi - kas 0, või MIN, vaatame
-        int[][] soidu_tulu = new[2][n + 1];
+        int[][] soidu_tulu_peatuses = new int[2][n + 1];
+        // Algväärtusta miinimumi peale
+        soidu_tulu_peatuses[0][0] = 0;
+        soidu_tulu_peatuses[1][0] = 0;
+
+        // Liigume üle peatuste
         for (int i = 1; i < n; i++) {
+            soidu_tulu_peatuses[0][i] = Integer.MIN_VALUE;
+            for(Tee tee : teed){
+                // Vaatame neid teid mis lõpevad käesolevas peatuses - ja leiame kõige tulusama
+                if(tee.lopp_peatus == i){
+                    // Alguspeatuses on meil juba arvestatud parim hind sinnani
+                    int kokku_tulu = soidu_tulu_peatuses[0][tee.algus_peatus] + peatuse_tulu[tee.lopp_peatus] - tee.soidu_kulu;
+                    soidu_tulu_peatuses[0][i] = Math.max(soidu_tulu_peatuses[0][i], kokku_tulu);
+                }
+            }
 
         }
 
