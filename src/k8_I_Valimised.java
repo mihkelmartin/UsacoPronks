8cf790582c4f7a85c88146f08ab2208c5c36a596
mihkel <mihkelmartin@gmail.com>
2021-09-28 17:32:37 +0300

    Bussireisid hea variant

--- src/k8_I_Valimised.java
+++ src/k8_I_Valimised.java
@@ -1,9 +1,9 @@
-import javax.xml.bind.annotation.adapters.CollapsedStringAdapter;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.HashMap;
 
 public class k8_I_Valimised {
     private static class Riik{
@@ -11,7 +11,7 @@ public class k8_I_Valimised {
         private int hind = 0;
         private String vanem;
         // -1 näitab, et ei ole veel kontrollitud
-        private int soltuvate_riikide_arv = -1;
+        private int haalte_arv = -1;
 
         public Riik(String nimi, int hind, String vanem) {
             this.nimi = nimi;
@@ -19,19 +19,18 @@ public class k8_I_Valimised {
             this.vanem = vanem;
         }
         private void leiaSoltuvateRiikideArv(ArrayList<Riik> riigid){
-            soltuvate_riikide_arv = 0;
+            haalte_arv = 1; // Vaikimisi 1
             for (Riik riik : riigid){
                 // On sõltuv riik
                 if(nimi.equals(riik.vanem)){
-                    soltuvate_riikide_arv++;
                     // Leia tema alamate hulk
-                    if(riik.soltuvate_riikide_arv == -1) {
+                    if(riik.haalte_arv == -1) {
                         riik.leiaSoltuvateRiikideArv(riigid);
                     } else {
                         int i = 0;
                     }
-                    this.soltuvate_riikide_arv += riik.soltuvate_riikide_arv;
-                    // Kuna riik ei saa olla mitmele vanem, siis katkestame
+                    this.haalte_arv += riik.haalte_arv;
+                    // Kuna riik ei saa olla mitmele vanem, siis võime katkestada
                     break;
                 }
             }
@@ -51,6 +50,14 @@ public class k8_I_Valimised {
         }
         Collections.sort(riigid, Comparator.comparingInt(riik -> riik.hind));
         riigid.forEach(riik -> riik.leiaSoltuvateRiikideArv(riigid));
+
+        // Indeks 0 on jällegi kui hääli on vaja 0
+        int[] maksta_haali = new int[m + 1];
+        HashMap<Integer, ArrayList<Riik>> kasutatud_riigid = new HashMap<>();
+        for (int i = 1; i <= m; i++) {
+
+        }
+
         System.out.println("OK");
     }
 }
