37a55c82f125c3cf4a4bf555f4d0045cf814a767
mihkel <mihkelmartin@gmail.com>
2021-09-28 21:09:56 +0300

    Valimised kõva variant

--- src/k8_I_Valimised.java
+++ src/k8_I_Valimised.java
@@ -6,6 +6,7 @@ import java.util.Comparator;
 import java.util.HashMap;
 
 public class k8_I_Valimised {
+
     private static class Riik{
         private String nimi;
         private int hind = 0;
@@ -19,14 +20,14 @@ public class k8_I_Valimised {
             this.hind = hind;
             this.vanem = vanem;
         }
-        private void leiaSoltuvateRiikideArv(ArrayList<Riik> riigid){
-            haalte_arv = 1; // Vaikimisi 1
+        private void leiaSoltuvateRiigid(ArrayList<Riik> riigid){
+            haalte_arv = 1; // Vaikimisi 1, käesolev riik ise
             for (Riik riik : riigid){
                 // On sõltuv riik
                 if(nimi.equals(riik.vanem)){
                     // Leia tema alamate hulk
                     if(riik.haalte_arv == -1) {
-                        riik.leiaSoltuvateRiikideArv(riigid);
+                        riik.leiaSoltuvateRiigid(riigid);
                     }
                     this.haalte_arv += riik.haalte_arv;
                     soltuvad_riigid.add(riik);
@@ -47,25 +48,38 @@ public class k8_I_Valimised {
             sisend = in.readLine().split(" ");
             riigid.add(new Riik(sisend[0], Integer.parseInt(sisend[1]), sisend[2].equals("0") ? null : sisend[3]));
         }
+        // Ei pea tegelikult sorteerima, aga õpi
         Collections.sort(riigid, Comparator.comparingInt(riik -> riik.hind));
-        riigid.forEach(riik -> riik.leiaSoltuvateRiikideArv(riigid));
+        // Nii on kõige lihtsam meetodit välja kutsuda
+        riigid.forEach(riik -> riik.leiaSoltuvateRiigid(riigid));
 
         // Indeks 0 on jällegi kui hääli on vaja 0
         int[] maksta_haali = new int[m + 1];
+        // Mingil häälte arvul juba kasutatud riigid
         HashMap<Integer, ArrayList<Riik>> kasutatud_riigid = new HashMap<>();
         // Lisa kohe 0 haalte puhul kasutatud riigid, mis on tühi hulk
         kasutatud_riigid.put(0, new ArrayList<>());
         int vastus = 0;
+
+        // Hakkame leidma dünaamiliselt parimat hinda alates 1
         for (int i = 1; i <= m; i++) {
+
             maksta_haali[i] = Integer.MAX_VALUE;
             ArrayList<Riik> riigid_kasutatud_kohal_i =  new ArrayList<>();
             kasutatud_riigid.put(i, riigid_kasutatud_kohal_i);
+
             for (Riik riik : riigid) {
+                // Kui kaugel on ettevõetud riik alguspunktist
                 int pos = Math.max(i - riik.haalte_arv, 0);
                 if(!kasutatud_riigid.get(pos).contains(riik)){
+                    // Mis kui on võrdne ?
                     if(maksta_haali[pos] + riik.hind < maksta_haali[i]){
+                        if(maksta_haali[pos] + riik.hind == maksta_haali[i]){
+                            int k=0;
+                        }
                         maksta_haali[i] = maksta_haali[pos] + riik.hind;
 
+                        // Uuendame riike mis on positsioonil kasutatud
                         riigid_kasutatud_kohal_i.clear();
                         riigid_kasutatud_kohal_i.addAll(kasutatud_riigid.get(pos));
                         riigid_kasutatud_kohal_i.add(riik);
@@ -75,6 +89,8 @@ public class k8_I_Valimised {
                     }
                 }
             }
+            // Ka kõik eelenvalt kasutatud tuleb listi panna
+            riigid_kasutatud_kohal_i.addAll(kasutatud_riigid.get(i-1));
         }
         System.out.println(vastus);
     }
