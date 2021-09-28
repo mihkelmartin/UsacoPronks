0fa1b4fc4cd39ce053b1e6fee47b03411fb9aed7
mihkel.martin <mihkelmartin@gmail.com>
2021-09-28 13:19:19 +0300

    Kolimine

--- src/k8_I_Valimised.java
+++ src/k8_I_Valimised.java
@@ -10,12 +10,33 @@ public class k8_I_Valimised {
         private String nimi;
         private int hind = 0;
         private String vanem;
+        // -1 näitab, et ei ole veel kontrollitud
+        private int soltuvate_riikide_arv = -1;
 
         public Riik(String nimi, int hind, String vanem) {
             this.nimi = nimi;
             this.hind = hind;
             this.vanem = vanem;
         }
+        private void leiaSoltuvateRiikideArv(ArrayList<Riik> riigid){
+            soltuvate_riikide_arv = 0;
+            for (Riik riik : riigid){
+                // On sõltuv riik
+                if(nimi.equals(riik.vanem)){
+                    soltuvate_riikide_arv++;
+                    // Leia tema alamate hulk
+                    if(riik.soltuvate_riikide_arv == -1) {
+                        riik.leiaSoltuvateRiikideArv(riigid);
+                    } else {
+                        int i = 0;
+                    }
+                    this.soltuvate_riikide_arv += riik.soltuvate_riikide_arv;
+                    // Kuna riik ei saa olla mitmele vanem, siis katkestame
+                    break;
+                }
+            }
+        }
+
     }
     public static void main(String[] args )throws Exception {
         InputStreamReader ina = new InputStreamReader(System.in);
@@ -29,6 +50,7 @@ public class k8_I_Valimised {
             riigid.add(new Riik(sisend[0], Integer.parseInt(sisend[1]), sisend[2].equals("0") ? null : sisend[3]));
         }
         Collections.sort(riigid, Comparator.comparingInt(riik -> riik.hind));
+        riigid.forEach(riik -> riik.leiaSoltuvateRiikideArv(riigid));
         System.out.println("OK");
     }
 }
