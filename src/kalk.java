735dd671fc9bdf8760eb9054e0106a6af503d348
mihkel <mihkelmartin@gmail.com>
2021-10-07 20:49:06 +0300

    lakl

--- src/kalk.java
+++ src/kalk.java
@@ -7,11 +7,13 @@ public class kalk {
     public static void main(String[] args )throws Exception {
         InputStreamReader ina = new InputStreamReader(System.in);
         BufferedReader in = new BufferedReader(ina);
+
         String avaldis = in.readLine();
+
         while (avaldis.indexOf(")") >=0){
             avaldis = arvuta_sulud(avaldis);
         }
-        //avaldis = arvuta_sulgudeta_avaldis(avaldis);
+        avaldis = arvuta_sulgudeta_avaldis(avaldis);
         System.out.println(avaldis);
     }
 
@@ -30,17 +32,66 @@ public class kalk {
         return retVal;
     }
     private static String arvuta_sulgudeta_avaldis(String avaldis){
-        String retVal = "proov";
+        String retVal = avaldis;
+        retVal = tee_tehted(retVal, '*', '/');
+        retVal = tee_tehted(retVal, '+', '-');
+        return retVal;
+    }
+
+    private static String tee_tehted(String avaldis, char tehe1, char tehe2){
+        String retVal = avaldis;
         // Korrutamised / Jagamised
-        for (int i = 0; i < avaldis.length(); i++) {
-            if(avaldis.charAt(i) == '*' || avaldis.charAt(i) == '/'){
-                int vasak_arv = leia_vasak_arv(avaldis, i);
-                int parem_arv = leia_parem_arv(avaldis, i);
-                String vasak_string = leia_vasak_string(avaldis, i);
-                String parem_string = leia_parem_string(avaldis, i);
+        boolean leiti = true;
+        while(leiti) {
+            leiti = false;
+            for (int i = 0; i < retVal.length(); i++) {
+                if (retVal.charAt(i) == tehe1 || retVal.charAt(i) == tehe2) {
+                    int vasak_arv = leia_vasak_arv(retVal, i);
+                    int parem_arv = leia_parem_arv(retVal, i);
+                    String vasak_string = leia_vasak_string(retVal, i);
+                    String parem_string = leia_parem_string(retVal, i);
+                    int tulemus = retVal.charAt(i) == '*' ? vasak_arv * parem_arv :
+                            retVal.charAt(i) == '/' ? vasak_arv / parem_arv :
+                                    retVal.charAt(i) == '+' ? vasak_arv + parem_arv : vasak_arv - parem_arv;
+                    retVal = vasak_string + tulemus + parem_string;
+                    leiti = true;
+                    break;
+                }
             }
         }
         return retVal;
+    }
+
+    private static String leia_parem_string(String avaldis, int i) {
+        int pos = i + 1;
+        while (avaldis.length() > pos && Character.isDigit(avaldis.charAt(pos))){
+            pos++;
+        }
+        return avaldis.substring(pos);
+    }
+
+    private static String leia_vasak_string(String avaldis, int i) {
+        int pos = i - 1;
+        while (pos >= 0 && Character.isDigit(avaldis.charAt(pos))){
+            pos--;
+        }
+        return avaldis.substring(0, pos + 1);
+    }
+
+    private static int leia_parem_arv(String avaldis, int i) {
+        int pos = i + 1;
+        while (avaldis.length() > pos && Character.isDigit(avaldis.charAt(pos))){
+            pos++;
+        }
+        return Integer.parseInt(avaldis.substring(i + 1, pos));
+    }
+
+    private static int leia_vasak_arv(String avaldis, int i) {
+        int pos = i - 1;
+        while (pos >= 0 && Character.isDigit(avaldis.charAt(pos))){
+            pos--;
+        }
+        return Integer.parseInt(avaldis.substring(pos + 1, i));
     }
 
 
