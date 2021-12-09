e9482d8d6dc67782a703ae5f2eb769c9cb63ec1e
mihkel.martin <mihkelmartin@gmail.com>
2021-12-09 09:39:49 +0200

    sulg

--- src/sulg.java
+++ src/sulg.java
@@ -11,8 +11,34 @@ public class sulg {
         String sone = in.readLine();
         for (int i = 0; i < qc; i++) {
             sisend = in.readLine().split(" ");
-            System.out.println("EI");
+            if(solve(sone, Integer.parseInt(sisend[0])-1, Integer.parseInt(sisend[1])))
+                System.out.println("JAH");
+            else
+                System.out.println("EI");
         }
+    }
+
+    private static boolean solve (String s, int l, int r) {
+        int lptr = l, rptr = r - 1;
+        int bal = 0, rbal = 0;
+        boolean seen0 = false;
+        while (lptr <= rptr) {
+            if (s.charAt(lptr) == '(') bal++;
+            else bal--;
+            if (bal < 0) return false;
+            if (bal == 0 && lptr != r - 1) seen0 = true;
+
+            if (lptr == rptr) break;
+
+            if (s.charAt(rptr) == '(') rbal++;
+            else rbal--;
+            if (rbal > 0) return false;
+            if (rbal == 0) seen0 = true;
+
+            lptr++;
+            rptr--;
+        }
+        return seen0 && bal + rbal == 0;
     }
 }
 
