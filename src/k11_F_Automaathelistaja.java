bb5c3a8f1489545f769138416a10f81fb9c51991
mihkel.martin <mihkelmartin@gmail.com>
2022-02-10 12:01:31 +0200

    Automaattvastaja

---
+++ src/k11_F_Automaathelistaja.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+import java.util.TreeSet;
+
+public class k11_F_Automaathelistaja {
+    public static void main(String[] args )throws Exception {
+
+        InputStreamReader ina = new InputStreamReader(System.in);
+        BufferedReader in = new BufferedReader(ina);
+        int numbreid = Integer.valueOf(in.readLine());
+        TreeSet<String>  numbrid = new TreeSet<>();
+        for (int i = 0; i < numbreid; i++) {
+            numbrid.add(in.readLine());
+        }
+        Iterator<String> iter = numbrid.iterator();
+        String eelmine = iter.next();
+        while (iter.hasNext()){
+            String praegune = iter.next();
+            if(praegune.substring(0, Math.min(praegune.length(), eelmine.length())).equals(eelmine)) {
+                System.out.println("EI");
+                System.exit(0);
+            }
+        }
+        System.out.println("JAH");
+    }
+}
